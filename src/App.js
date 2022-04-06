af3d878a9d24b485d04fb23223f7caa665ce8d8c
Akbaraliii <91048215+Akbaraliii@users.noreply.github.com>
2022-04-06 23:39:04 +0900

    Dynamic approach and final one

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import Menu from './Menu'
 import Categories from './Categories'
 import items from './data'
 
+const allCatagories = ['all', ...new Set(items.map((item) => item.category))]
+console.log(allCatagories)
+
 function App() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState(allCatagories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
   return (
     <section className='menu section'>
       <div className='title'>
         <h2>our menu</h2>
         <div className='underline'></div>
       </div>
-      <Categories />
+      <Categories categories={categories} filterItems={filterItems} />
       <Menu items={menuItems} />
     </section>
   )
